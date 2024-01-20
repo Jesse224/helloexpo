672becb256e08da5a89eeb5c432c594749b995d1
unknown <jeze363@gmail.com>
2024-01-20 15:35:33 +0200

    Tehtävää täydennetty

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Moi, miten menee?</Text>
+      <Text>Moi, miten menee? Testi</Text>
       <StatusBar style="auto" />
     </View>
   );
